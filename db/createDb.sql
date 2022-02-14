afdd7a971c36e96deca8731d5dd1a272a59c56f2
fifthaccess <benedikt.erkner-sach@bulme.at>
2022-02-14 12:51:39 +0100

    Sql_update

--- db/createDb.sql
+++ db/createDb.sql
@@ -1,14 +1,12 @@
-/*create database if not exists Projekt_06;
+create database if not exists Projekt_06;
 use Projekt_06;
 
-create table if not exists Künstler (
-	KünstlerId int auto_increment unique key primary key,
-	Gehalt int (40),
-	Herkunftsland varchar (40),
-	Vorname varchar (40),
-	Nachname varchar (40)
-);
-
+create table if not exists Lied_Künstler 
+( 
+	Id int auto_increment unique key primary key,
+    kÜnstlerId int (40),
+    LiedId int (40)
+    );
 create table if not exists Lied (
 	LiedId int auto_increment unique key primary key,
 	Künsterzahl int (40),
@@ -23,4 +21,23 @@ create table if not exists Manager (
 	Nachname varchar (40),
 	Firma varchar (40),
 	Künstler_anzahl int (40) 
+);
+
+create table if not exists Lied_Künstler 
+( 
+	Id int auto_increment unique key primary key,
+    KünstlerId int (40),
+    LiedId int (40),
+    constraint KünstlerForeignKey foreign key (KünstlerId) references Künstler (KünstlerId),
+    constraint LiedForeignKey foreign key (LiedId) references Lied (LiedId)
+    );
+    
+create table if not exists Künstler (
+	KünstlerId int auto_increment unique key primary key,
+	Gehalt int (40),
+	Herkunftsland varchar (40),
+	Vorname varchar (40),
+	Nachname varchar (40),
+    ManagerId int (40),
+    constraint mangerForeignKey foreign key (ManagerId) references Manager (ManagerId)
 );
