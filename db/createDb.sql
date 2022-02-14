6db57df6f83b562c7208a5c1012c68b6cfd7289c
fifthaccess <benedikt.erkner-sach@bulme.at>
2022-02-14 12:58:17 +0100

    Sql update

--- db/createDb.sql
+++ db/createDb.sql
@@ -1,16 +1,16 @@
 create database if not exists Projekt_06;
 use Projekt_06;
 
-create table if not exists Lied_Künstler 
+create table if not exists Lied_Kuenstler 
 ( 
 	Id int auto_increment unique key primary key,
-    kÜnstlerId int (40),
+    kuenstlerId int (40),
     LiedId int (40)
     );
 create table if not exists Lied (
 	LiedId int auto_increment unique key primary key,
-	Künsterzahl int (40),
-	KünsterlId int (40),
+	Kuensterzahl int (40),
+	KuensterlId int (40),
 	Liedname varchar (40),
 	Erscheinungsdatum date 
 );
@@ -20,20 +20,20 @@ create table if not exists Manager (
 	Vorname varchar (40),
 	Nachname varchar (40),
 	Firma varchar (40),
-	Künstler_anzahl int (40) 
+	Kuenstler_anzahl int (40) 
 );
 
-create table if not exists Lied_Künstler 
+create table if not exists Lied_Kuenstler 
 ( 
 	Id int auto_increment unique key primary key,
-    KünstlerId int (40),
+    KuenstlerId int (40),
     LiedId int (40),
-    constraint KünstlerForeignKey foreign key (KünstlerId) references Künstler (KünstlerId),
+    constraint KuenstlerForeignKey foreign key (KuenstlerId) references Kuenstler (KuenstlerId),
     constraint LiedForeignKey foreign key (LiedId) references Lied (LiedId)
     );
     
-create table if not exists Künstler (
-	KünstlerId int auto_increment unique key primary key,
+create table if not exists Kuenstler (
+	KuenstlerId int auto_increment unique key primary key,
 	Gehalt int (40),
 	Herkunftsland varchar (40),
 	Vorname varchar (40),
