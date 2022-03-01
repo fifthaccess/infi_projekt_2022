241c45f7c851fcbcc433571372b45273ad3d7d8f
fifthaccess <benedikt.erkner-sach@bulme.at>
2022-03-01 09:16:20 +0100

    app v1.1

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from model import Todoitem, db
 app = Flask(__name__)
-app.secret_key = "fifth0"
+app.secret_key = "fifth"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/Projekt_06"
+
 db.init_app(app)
 
 #test = Todoitem
