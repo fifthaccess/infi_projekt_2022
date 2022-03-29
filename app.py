3266ffd3f30aa18e5b80c0adc74b1429e1362366
fifthaccess <benedikt.erkner-sach@bulme.at>
2022-03-29 09:54:57 +0200

    updating app

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
-from models import Kuenstler,Lied,LiedKuenstler,Manager,db
+from model.models import Kuenstler,Lied,LiedKuenstler,Manager,db
 from flask.templating import render_template
-from addItemForm import AddItemForm
+#from addItemForm import AddItemForm
+from controllers.index import index_blueprint
+from controllers.manager import manager_blueprint
 app = Flask(__name__)
 app.secret_key = "fifth"
 
@@ -9,11 +11,12 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/Projekt_06"
 
 db.init_app(app)
-@app.route("/", methods=["get","post"])
-def index():
-
-    items = db.session.query(Lied).all()
-    return render_template("index.html", headline="Übersicht")#, form = addItemFormObject, items = items)
+#@app.route("/", methods=["get","post"])
+#def index():
+#    items = db.session.query(Lied).all()
+#    return render_template("index.html", headline="Übersicht")#, form = addItemFormObject, items = items)
     
-
-app.run(debug=True)
+app.register_blueprint(index_blueprint)
+app.register_blueprint(manager_blueprint)
+ 
+app.run(debug=True)     
