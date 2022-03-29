39501bcacfb658f37a3e7f43ac425f2cdb18ab9c
fifthaccess <benedikt.erkner-sach@bulme.at>
2022-03-29 09:55:52 +0200

    creating manager class

---
+++ forms/Manager.py
@@ -0,0 +1,13 @@
+from operator import imod
+from flask_wtf import FlaskForm
+from wtforms.fields.datetime import DateField
+from wtforms.fields.simple import BooleanField, StringField, TextAreaField, DecimalField
+from wtforms import validators
+from model.models import Kuenstler
+
+class Manager(FlaskForm):
+    ManagerID = StringField("ManagerID",[validators.InputRequired()])
+    Vorname = StringField("Vorname")
+    Nachname = StringField("Nachname")
+    Firma = StringField("Firma")
+    Kuenstler_anzahl = DecimalField("Kuenstler_anzahl")
