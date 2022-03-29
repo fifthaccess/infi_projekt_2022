f53ac4dbb8bb2c9a6b68b7dc43e59c51391a16bc
fifthaccess <benedikt.erkner-sach@bulme.at>
2022-03-29 10:30:33 +0200

    update app

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ db.init_app(app)
     
 app.register_blueprint(index_blueprint)
 app.register_blueprint(manager_blueprint)
- 
-app.run(debug=True)     
+if __name__ == "__main__": 
+    app.run(debug=True)     
