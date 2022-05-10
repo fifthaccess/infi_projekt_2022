c14acd4626b84ccaecc111908a8c07477f95b38f
fifthaccess <benedikt.erkner-sach@bulme.at>
2022-05-10 08:47:55 +0200

    removing useless code

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from model.models import Kuenstler,Lied,LiedKuenstler,Manager,db
+from model.models import db
 from flask.templating import render_template
 #from addItemForm import AddItemForm
 from controllers.index import index_blueprint
 from controllers.manager import manager_blueprint
 from controllers.kuenstler import kuenstler_blueprint
+from controllers.lied import lied_blueprint
 app = Flask(__name__)
 app.secret_key = "fifth"
 
@@ -12,13 +13,10 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/Projekt_06"
 
 db.init_app(app)
-#@app.route("/", methods=["get","post"])
-#def index():
-#    items = db.session.query(Lied).all()
-#    return render_template("index.html", headline="Übersicht")#, form = addItemFormObject, items = items)
-    
+
 app.register_blueprint(index_blueprint)
 app.register_blueprint(manager_blueprint)
 app.register_blueprint(kuenstler_blueprint)
+app.register_blueprint(lied_blueprint)
 if __name__ == "__main__": 
     app.run(debug=True)     
